286ce8346c674327d7582f3f185843d3816d8f63
Borislav Chernev <borocool359@gmail.com>
2024-04-01 23:12:05 +0300

    implemented guide services with added error handling

--- gaming-catalog-app/src/app/core/guide/guide.service.ts
+++ gaming-catalog-app/src/app/core/guide/guide.service.ts
@@ -1,9 +1,83 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  DocumentReference,
+} from '@angular/fire/compat/firestore';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Guide } from 'src/app/interfaces/guide.interface';
+import { VALIDATION_MESSAGES } from 'src/app/shared/constants/validation.errors';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GuideService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  getAllGuides(): Observable<Guide[]> {
+    return this.firestore
+      .collection<Guide>('guides')
+      .valueChanges()
+      .pipe(
+        catchError((error) => {
+          console.error(VALIDATION_MESSAGES.GUIDE.GET_ALL_ERROR, error);
+          return throwError(
+            () => new Error(VALIDATION_MESSAGES.GUIDE.GET_ALL_ERROR)
+          );
+        })
+      );
+  }
+
+  getGuideDetailsById(GuideId: string): Observable<Guide | undefined> {
+    return this.firestore
+      .collection<Guide>('guides')
+      .doc<Guide>(GuideId)
+      .valueChanges()
+      .pipe(
+        catchError((error) => {
+          console.error(
+            VALIDATION_MESSAGES.GUIDE.GET_DETAILS_BY_ID_ERROR.replace(
+              '%s',
+              GuideId
+            ),
+            error
+          );
+          return throwError(
+            () =>
+              new Error(
+                VALIDATION_MESSAGES.GUIDE.GET_DETAILS_BY_ID_ERROR.replace(
+                  '%s',
+                  GuideId
+                )
+              )
+          );
+        })
+      );
+  }
+
+  createNewGuide(Guide: Guide): Promise<DocumentReference<Guide>> {
+    return this.firestore
+      .collection<Guide>('guides')
+      .add(Guide)
+      .catch((error) => {
+        console.error(VALIDATION_MESSAGES.GUIDE.CREATE_NEW_ERROR, error);
+        throw new Error(VALIDATION_MESSAGES.GUIDE.CREATE_NEW_ERROR);
+      });
+  }
+
+  editGuideById(
+    GuideId: string,
+    updatedGuideData: Partial<Guide>
+  ): Promise<void> {
+    return this.firestore
+      .collection<Guide>('guides')
+      .doc(GuideId)
+      .update(updatedGuideData)
+      .catch((error) => {
+        console.error(
+          VALIDATION_MESSAGES.GUIDE.EDIT_BY_ID_ERROR.replace('%s', GuideId),
+          error
+        );
+        throw new Error(VALIDATION_MESSAGES.GUIDE.EDIT_BY_ID_ERROR);
+      });
+  }
 }
--- gaming-catalog-app/src/app/shared/constants/validation.errors.ts
+++ gaming-catalog-app/src/app/shared/constants/validation.errors.ts
@@ -19,10 +19,13 @@ export const VALIDATION_MESSAGES = {
     DELETE_ERROR: 'Failed to delete discussion. Please try again later.',
   },
   GUIDE: {
-    GET_ALL_ERROR: '',
-    GET_DETAILS_BY_ID_ERROR: '',
-    CREATE_NEW_ERROR: '',
-    EDIT_BY_ID_ERROR: '',
-    DELETE_ERROR: '',
+    GET_ALL_ERROR:
+      'Something went wrong while fetching all guides. Please try again later.',
+    GET_DETAILS_BY_ID_ERROR:
+      'Something went wrong while fetching details for guide with ID %s. Please try again later.',
+    CREATE_NEW_ERROR: 'Failed to create new guide. Please try again later',
+    EDIT_BY_ID_ERROR:
+      'Failed to edit guide with ID %s. Please try again later.',
+    DELETE_ERROR: 'Failed to delete guide. Please try again later.',
   },
 };
