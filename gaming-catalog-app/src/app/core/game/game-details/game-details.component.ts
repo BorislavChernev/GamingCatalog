e27f31e9ad77cde8f241e48645f2a0e33d885632
Borislav Chernev <borocool359@gmail.com>
2024-04-02 22:10:32 +0300

    workflow edited for both back-end and front-end build

--- gaming-catalog-app/src/app/core/game/game-details/game-details.component.ts
+++ gaming-catalog-app/src/app/core/game/game-details/game-details.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-game-details',
   templateUrl: './game-details.component.html',
   styleUrls: ['./game-details.component.css'],
 })
-export class GameDetailsComponent {}
+export class GameDetailsComponent implements OnInit {
+  game: any; // Adjust according to your data model
+
+  constructor(
+    private route: ActivatedRoute,
+    private gameService: GameService
+  ) {}
+
+  ngOnInit(): void {
+    const gameId = this.route.snapshot.paramMap.get('id');
+    if (gameId) {
+      this.gameService.getGameDetailsById(gameId).subscribe({
+        next: (game) => {
+          this.game = game; // Assuming `getGameDetailsById` fetches the game data based on `id`
+        },
+        error: (error) => {
+          console.error('Error fetching game details:', error);
+        },
+      });
+    }
+  }
+}
